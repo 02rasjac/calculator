cceb0ece183c03bcec14740128f4667eb2702ec3
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2023-03-12 12:43:27 +0100

    Refactor update-function

--- calc.js
+++ calc.js
@@ -2,24 +2,28 @@ let out = document.querySelector('#large');
 let fullOut = document.querySelector('#small');
 let buttons = document.querySelectorAll('button');
 buttons.forEach((b) => {
-  b.addEventListener('click', Update);
+  if (b.dataset.val != null) b.addEventListener('click', UpdateVal);
+  else if (b.dataset.op != null) b.addEventListener('click', UpdateOp);
+  else if (b.dataset.func != null) b.addEventListener('click', UpdateFunc);
 });
 
 let prevOp = null;
 
-function Update(e) {
-  if (e.target.dataset.val != null) {
-    out.textContent += e.target.dataset.val;
-  } else if (e.target.dataset.op != null) {
-    const currOp = e.target.dataset.op;
-    out.textContent += currOp;
-    let arr = out.textContent.split(/[+\-*/]+/);
-    if (arr.length === 3) {
-      out.textContent = Operate(Number(arr[0]), Number(arr[1]), prevOp) + currOp;
-    }
-    prevOp = currOp;
-  }
+function UpdateVal(e) {
+  out.textContent += e.target.dataset.val;
 }
+
+function UpdateOp(e) {
+  const currOp = e.target.dataset.op;
+  out.textContent += currOp;
+  let arr = out.textContent.split(/[+\-*/]+/);
+  if (arr.length === 3) {
+    out.textContent = Operate(Number(arr[0]), Number(arr[1]), prevOp) + currOp;
+  }
+  prevOp = currOp;
+}
+
+function UpdateFunc(e) {}
 
 function Operate(a, b, op) {
   switch (op) {
